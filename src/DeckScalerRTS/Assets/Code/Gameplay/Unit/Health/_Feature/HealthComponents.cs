d144542caa58dd8c67d4aa5e188ea61ba2019abf
rapuska <mikalajsiatkouski@gmail.com>
2025-03-05 16:50:54 +0300

    progress bar

--- src/DeckScalerRTS/Assets/Code/Gameplay/Unit/Health/_Feature/HealthComponents.cs
+++ src/DeckScalerRTS/Assets/Code/Gameplay/Unit/Health/_Feature/HealthComponents.cs
@@ -2,9 +2,9 @@ using Entitas.Generic;
 
 namespace DeckScaler
 {
-    public sealed class MaxHealth : ValueComponent<float>, IInScope<GameScope> { }
+    public sealed class MaxHealth : ValueComponent<float>, IInScope<GameScope>, IEvent<Self> { }
 
-    public sealed class Health : ValueComponent<float>, IInScope<GameScope> { }
+    public sealed class Health : ValueComponent<float>, IInScope<GameScope>, IEvent<Self> { }
 
     public sealed class Dead : FlagComponent, IInScope<GameScope> { }
 }
--- src/DeckScalerRTS/Assets/Code/Utils/Extensions/MathExtensions.cs
+++ src/DeckScalerRTS/Assets/Code/Utils/Extensions/MathExtensions.cs
@@ -4,6 +4,9 @@ namespace DeckScaler
 {
     public static class MathExtensions
     {
+        public static float Clamp01(this float @this)
+            => Mathf.Clamp01(@this);
+
         public static float Clamp(this float @this, float? min = null, float? max = null)
             => Mathf.Clamp(@this, min ?? @this, max ?? @this);
 
