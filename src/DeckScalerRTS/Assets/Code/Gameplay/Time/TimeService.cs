46b73eedb47e530f9ea9191fc7ec1ff2e0cdaa45
rapuska <mikalajsiatkouski@gmail.com>
2025-04-08 22:58:39 +0300

    don't use ability if time is stopped

--- src/DeckScalerRTS/Assets/Code/Gameplay/Time/TimeService.cs
+++ src/DeckScalerRTS/Assets/Code/Gameplay/Time/TimeService.cs
@@ -4,6 +4,8 @@ namespace DeckScaler
 {
     public interface ITimeService : IService
     {
+        bool IsTimeStopped { get; }
+
         float GameplayDelta { get; }
 
         float RealDelta { get; }
@@ -15,19 +17,20 @@ namespace DeckScaler
 
     public class TimeService : ITimeService
     {
-        private bool _isTimeStopEnabled;
         private float? _timeScale;
 
-        public float GameplayDelta => _isTimeStopEnabled
+        public float GameplayDelta => IsTimeStopped
             ? 0
             : Time.deltaTime * TimeScale;
 
         public float RealDelta => Time.deltaTime;
 
+        public bool IsTimeStopped { get; private set; }
+
         private float TimeScale => _timeScale ?? 1f;
 
         public void ToggleTimeStop()
-            => _isTimeStopEnabled = !_isTimeStopEnabled;
+            => IsTimeStopped = !IsTimeStopped;
 
         public void ToggleSpeedUp()
             => _timeScale = _timeScale.HasValue ? null : 2f;
